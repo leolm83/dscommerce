37be3863630b82426dd6017fa1a118e736ac2458
leolm83 <leonardo.mateus01@hotmail.com>
2024-06-23 23:43:23 -0300

    inicio do projeto

---
+++ src/main/java/com/leolmcoding/dscommerce/entities/Order.java
@@ -0,0 +1,73 @@
+package com.leolmcoding.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+@Entity
+@Table(name = "tb_order")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant moment;
+    private OrderStatus orderStatus;
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
+
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client, Payment payment) {
+        this.id = id;
+        this.moment = moment;
+        this.orderStatus = orderStatus;
+        this.client = client;
+        this.payment = payment;
+    }
+
+    public Order() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
+
+    @OneToOne(mappedBy = "")
+    private Payment payment;
+}
---
+++ src/main/java/com/leolmcoding/dscommerce/entities/User.java
@@ -0,0 +1,39 @@
+package com.leolmcoding.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDate birthDate;
+    private String password;
+    private String phone;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, LocalDate birthDate, String password, String phone) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.password = password;
+        this.phone = phone;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+}
